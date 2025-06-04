128e74ecf315c39f4f58b08dd9bee11ce4c05d83
u0140201 <wouter.vanassche@kuleuven.be>
2025-06-04 12:05:06 +0200

    add tests for use of the VSC_RPROJECT_CONFIG environment variable

--- testing/test_vsc_rproject.sh
+++ testing/test_vsc_rproject.sh
@@ -41,7 +41,7 @@ setup() {
 
   DEFAULT_CRAN="https://cloud.r-project.org"
   CRAN="https://ftp.belnet.be/mirror/CRAN/"
-  
+
   DEFAULT_MARCH="x86-64-v4"
   MARCH="skylake"
 
@@ -55,15 +55,15 @@ setup() {
   BAD_MODULES_FILE="$TEST_DIR/.bad_modules.txt"
   echo "JAGS/4.3.2-foss-2023b" > "$BAD_MODULES_FILE" # no R module in the modules file
 
-  VSC_RPROJECTS_CONFIG="${VSC_HOME}/.vsc-rproject-config"
-  if [[ -f "${VSC_RPROJECTS_CONFIG}" ]]; then
+  TEST_CONFIG="${VSC_HOME}/.vsc-rproject-config"
+  if [[ -f "${TEST_CONFIG}" ]]; then
     echo "storing user .vsc-rproject-config file..."
     USER_CONFIG="${VSC_HOME}/.vsc-rproject-config-user"
-    mv "${VSC_RPROJECTS_CONFIG}" "${USER_CONFIG}"
+    mv "${TEST_CONFIG}" "${USER_CONFIG}"
   fi
 
   cd "${TEST_DIR}" || exit 1
-  
+
   echo "cleaning up modules"
   module purge &> /dev/null
 
@@ -83,7 +83,7 @@ cleanup() {
 
   if [[ -f "${USER_CONFIG}" ]]; then
     echo "restoring user .vsc-rproject-config file..."
-    mv "${USER_CONFIG}" "${VSC_RPROJECTS_CONFIG}"
+    mv "${USER_CONFIG}" "${TEST_CONFIG}"
   fi
 
   echo "cleaning up modules"
@@ -96,9 +96,9 @@ run_test() {
   echo_test() {
     echo -e "${WHITE}${1}${NC}"
   }
-  
+
   echo_pass() {
-    echo -e "${GREEN}  ${1} ✅${NC}" 
+    echo -e "${GREEN}  ${1} ✅${NC}"
   }
 
   echo_fail() {
@@ -109,7 +109,7 @@ run_test() {
   local command
   local expected_status=0
   local expected_output
-  
+
   # Parse arguments
   while [[ $# -gt 0 ]]; do
     case "$1" in
@@ -141,8 +141,8 @@ run_test() {
   write_log "=========================================================="
   write_log "TEST: $test_name"
   local result
-  
-  result=$( 
+
+  result=$(
     eval "$command"
     echo "::STATUS::$?"
   )
@@ -160,7 +160,7 @@ run_test() {
     echo_fail "status"
     write_log "FAIL: $test_name - status"
     write_log "  status: $status"
-    write_log "  expected: $expected_status" 
+    write_log "  expected: $expected_status"
   fi
   if [ -n "$expected_output" ]; then
     if echo "$output" | grep -q "$expected_output"; then
@@ -186,9 +186,9 @@ main_tests() {
   write_log ""
 
   # --- positive tests --- #
-  run_test -n "vsc-Rproject help" -c "vsc-rproject --help" 
+  run_test -n "vsc-Rproject help" -c "vsc-rproject --help"
   run_test -n "vsc-Rproject version" -c "vsc-rproject --version" -o "${VERSION}"
-  run_test -n "vsc-Rproject defaults" -c "vsc-rproject --defaults"
+  run_test -n "vsc-Rproject defaults | with ${TEST_CONFIG}" -c "vsc-rproject --defaults" -o "${TEST_CONFIG}"
   run_test -n "- check: R" -c "vsc-rproject --defaults" -o "${DEFAULT_R_MODULE}"
   run_test -n "- check: location" -c "vsc-rproject --defaults" -o "${TRUE_DEFAULT_LOCATION}"
   run_test -n "- check: CRAN" -c "vsc-rproject --defaults" -o "${DEFAULT_CRAN}"
@@ -219,17 +219,31 @@ configure_tests() {
   run_test -n "Configure default march" -c 'vsc-rproject configure -M ${MARCH}' -o "Set default_march to: ${MARCH}"
 
   # check if new defaults are shown with vsc-rproject --defaults
-  run_test -n "vsc-Rproject updated defaults" -c "vsc-rproject --defaults"
+  run_test -n "vsc-Rproject updated defaults" -c "vsc-rproject --defaults" -o "${TEST_CONFIG}"
   run_test -n "- check: updated R" -c 'vsc-rproject --defaults' -o "${R_MODULE}"
   run_test -n "- check: updated location" -c 'vsc-rproject --defaults' -o "${TRUE_LOCATION}"
   run_test -n "- check: updated CRAN" -c 'vsc-rproject --defaults' -o "${CRAN}"
   run_test -n "- check: updated march" -c 'vsc-rproject --defaults' -o "${MARCH}"
+  run_test -n "- check: content of ${TEST_CONFIG}" -c "cat ${TEST_CONFIG}" -o "${R_MODULE}"
+
 
   # resetting defaults
-  run_test -n "Configure reset defaults" -c 'vsc-rproject configure --reset <<< "yes"' -o "vsc-Rproject settings have been reset"
-  run_test -n "- check: config file empty" -c '! [[ -s "${VSC_RPROJECTS_CONFIG}" ]]'
-
+  run_test -n "Configure reset ${TEST_CONFIG}" -c 'vsc-rproject configure --reset <<< "yes"' -o "vsc-Rproject settings have been reset"
+  run_test -n "- check: ${TEST_CONFIG} empty" -c '! [[ -s "${TEST_CONFIG}" ]]'
   run_test -n "Configure reset | already default settings" -c 'vsc-rproject configure -r <<< "yes"' -o "vsc-Rproject settings already at default"
+
+  # use alternative config via $VSC_RPROJECT_CONFIG
+  VSC_RPROJECT_CONFIG=~/.vsc-rproject-config-test
+  touch "${VSC_RPROJECT_CONFIG}"
+
+  run_test -n "vsc-Rproject defaults | with ${VSC_RPROJECT_CONFIG}" -c 'vsc-rproject --defaults' -o "${VSC_RPROJECT_CONFIG}"
+  run_test -n "Configure default location in ${VSC_RPROJECT_CONFIG}" -c 'vsc-rproject configure -l ${LOCATION}' -o "Set default_project_location to: ${TRUE_LOCATION}"
+  run_test -n "- check: content of ${VSC_RPROJECT_CONFIG}" -c 'cat ${VSC_RPROJECT_CONFIG}' -o "${TRUE_LOCATION}"
+  run_test -n "Configure reset | ${VSC_RPROJECT_CONFIG}" -c 'vsc-rproject configure -r <<< "yes"' -o "vsc-Rproject settings have been reset"
+  run_test -n "- check: $VSC_RPROJECT_CONFIG empty" -c '! [[ -s "${VSC_RPROJECT_CONFIG}" ]]'
+
+  rm $VSC_RPROJECT_CONFIG
+  unset VSC_RPROJECT_CONFIG
 
   # --- negative tests --- #
   run_test -n "Configure default with non-existing R" -c 'vsc-rproject configure -R ${BAD_R_MODULE}' -s 1 -o "${BAD_R_MODULE} not found."
@@ -247,7 +261,7 @@ create_tests() {
   write_log ""
 
   # --- positive tests --- #
-  # vsc-rproject create --help 
+  # vsc-rproject create --help
   run_test -n "Create help" -c "vsc-rproject create --help"
   run_test -n "- check: default R" -c "vsc-rproject create --help" -o "${DEFAULT_R_MODULE}"
   run_test -n "- check: default location" -c "vsc-rproject create --help" -o "${TRUE_DEFAULT_LOCATION}"
@@ -257,7 +271,7 @@ create_tests() {
 
   # creating a vsc-Rproject
   run_test -n "Create project" -c 'vsc-rproject create "${NAME1}" -l "${LOCATION}"' -o "vsc-Rproject environment setup complete"
-  run_test -n "- check: project_root exists" -c '[[ -d "${LOCATION}/${NAME1}" ]]' 
+  run_test -n "- check: project_root exists" -c '[[ -d "${LOCATION}/${NAME1}" ]]'
   run_test -n "- check: Rlibrary exists" -c '[[ -d "${LOCATION}/${NAME1}/library/${VSC_OS_LOCAL}/R" ]]'
   run_test -n "- check: .Rprofile content" -c 'cat "${LOCATION}/${NAME1}/.Rprofile"' -o "options(repos = c(CRAN = \"${DEFAULT_CRAN}\"))"
   run_test -n "- check: .Renviron content" -c 'cat "${LOCATION}/${NAME1}/.Renviron"' -o "R_LIBS_USER=\"${TRUE_LOCATION}/${NAME1}/library/\${VSC_OS_LOCAL}/R\""
@@ -318,11 +332,11 @@ activate_tests() {
 
   # --- positive tests --- #
   # vsc-rproject activate --help
-  run_test -n "Activate help" -c 'vsc-rproject activate --help' -o "${TRUE_DEFAULT_LOCATION}" 
+  run_test -n "Activate help" -c 'vsc-rproject activate --help' -o "${TRUE_DEFAULT_LOCATION}"
   run_test -n "Activate help - additional arg" -c 'vsc-rproject activate --location=${LOCATION} --help' -o "${TRUE_DEFAULT_LOCATION}"
 
   # activating a vsc-Rproject
-  
+
   run_test -n "Activate project_1 | default settings" -c 'vsc-rproject activate "${NAME1}" -l "${LOCATION}"' -s 0 -o "${NAME1} activated"
   run_test -n "- check: \$VSC_RPROJECT" -c 'vsc-rproject activate "${NAME1}" -l "${LOCATION}"; echo $VSC_RPROJECT' -o "${TRUE_LOCATION}/${NAME1}"
   run_test -n "- check: R module loaded" -c 'vsc-rproject activate "${NAME1}" -l "${LOCATION}"; echo $(module --terse --redirect list)' -o "${DEFAULT_R_MODULE}"
@@ -361,10 +375,10 @@ deactivate_tests() {
 # Parsing Tests Cases #
 # =================== #
 
-setup 
+setup
 trap cleanup EXIT
 echo "==============="
-echo " Running Tests " 
+echo " Running Tests "
 echo "==============="
 
 if [[ $# -eq 0 ]]; then
