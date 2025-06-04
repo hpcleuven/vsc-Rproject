f785736211579f157f53d015efd965035923768a
u0140201 <wouter.vanassche@kuleuven.be>
2025-06-04 16:29:35 +0200

    restructuring

--- setup.sh
+++ setup.sh
@@ -1,15 +1,20 @@
 #!/bin/bash -l
 
+VERSION=1.0.0
+
 GIT_ROOT="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
-mkdir -p "${VSC_DATA}/.local/apps/"
-cp -R "${GIT_ROOT}/src/vsc-Rproject" "${VSC_DATA}/.local/apps/" 
+APPDIR="${VSC_DATA}/.local/apps/vsc-Rproject/${VERSION}"
+MODDIR="${VSC_DATA}/.local/modules/"
 
-mkdir -p "${VSC_DATA}/.local/modules"
-cp -R "${GIT_ROOT}/module_file/vsc-Rproject" "${VSC_DATA}/.local/modules"
+mkdir -p "${APPDIR}"
+mkdir -p "${MODDIR}"
+
+cp -R "${GIT_ROOT}/libexec" "${APPDIR}" 
+cp -R "${GIT_ROOT}/module_file/vsc-Rproject" "${MODDIR}"
 
 # Making the module available via .bashrc
-echo "execute following line manually to make the module available:"
+echo 'execute following line manually to make the module available:'
 echo 'export MODULEPATH="${VSC_DATA}/.local/modules:${MODULEPATH}"'
 
 #echo 'export MODULEPATH="${VSC_DATA}/.local/modules:${MODULEPATH}"' >> ~/.bashrc
